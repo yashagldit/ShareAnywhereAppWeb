5e2b2fd1f6ef21eaab9e59c4725232c28c459b57
Yash Agarwal <yashagl1620@gmail.com>
2022-06-09 23:14:12 +0530

    test

--- worker-basic.min.js
+++ worker-basic.min.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
@@ -8,10 +8,34 @@
 
 // tslint:disable:no-console
 
-self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
 
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
-  self.registration.unregister().then(
-      () => { console.log('NGSW Safety Worker - unregistered old service worker'); });
+
+  event.waitUntil(self.registration.unregister().then(() => {
+    console.log('NGSW Safety Worker - unregistered old service worker');
+  }));
+
+  event.waitUntil(caches.keys().then(cacheNames => {
+    const ngswCacheNames = cacheNames.filter(name => /^ngsw:/.test(name));
+    return Promise.all(ngswCacheNames.map(name => caches.delete(name)));
+  }));
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  // If this is an incoming POST request for the
+  // registered "action" URL, respond to it.
+  if (event.request.method === 'POST' ) {
+    event.respondWith((async () => {
+      const formData = await event.request.formData();
+      const link = formData.get('link') || '';
+      console.log(formData);
+    //  const responseUrl = await saveBookmark(link);
+    //  return Response.redirect(responseUrl, 303);
+    })());
+  }
 });
